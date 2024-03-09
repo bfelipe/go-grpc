9b8dcccfd7cf74c9bc975be2578c508216edd3bb
Bruno Felipe <cs.felipebm@gmail.com>
2024-03-09 14:11:46 -0300

    add server reflection for clients

--- simple-server/cmd/server.go
+++ simple-server/cmd/server.go
@@ -7,6 +7,7 @@ import (
 	"gitlab.com/bfelipe/go-grpc/simple-server/pb"
 	"gitlab.com/bfelipe/go-grpc/simple-server/service"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -19,6 +20,7 @@ func main() {
 	server := grpc.NewServer()
 	var helloService service.HelloService
 	pb.RegisterHelloServer(server, &helloService)
+	reflection.Register(server)
 
 	log.Printf("info: listening on %s", addr)
 	if err := server.Serve(l); err != nil {
