1deb9feb264ef1c5e1161a8f380db9b2142699ca
Bruno Felipe <cs.felipebm@gmail.com>
2024-03-10 19:41:30 -0300

    add tools for grpc gateway

--- tools/gen.sh
+++ tools/gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc $1 -I=$GOPATH/src/{project-name} --go_out=$GOPATH/src/{project-name} --go-grpc_out=$GOPATH/src/{project-name} --proto_path=$GOPATH/src/{project-name}/subpath-to-protofile
+protoc $1 -I=$GOPATH/src/{project-name} --go_out=$GOPATH/src/{project-name} --go-grpc_out=$GOPATH/src/{project-name} --proto_path=$GOPATH/src/{project-name}/{subpath-to-protofile}
